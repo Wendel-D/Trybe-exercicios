c2ee79ffb998530785ed24844a66654a0ae24230
Wendel Daniel <wendeldaniel263@gmail.com>
2022-08-04 18:55:49 -0300

    novo bloco

---
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicios.js
@@ -0,0 +1,85 @@
+//1 - Dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
+
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+console.log(planetDistanceFromSun(venus)); // B
+console.log(planetDistanceFromSun(jupiter)); // C
+
+//RESPOSTA = A - B - C
+
+//2 - Agora, dado o código abaixo, qual a ordem de finalização de execução das linhas comentadas?
+
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+console.log(planetDistanceFromSun(mars)); // A
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
+
+//RESPOSTA = A - C - B 
+
+//3 - A função getPlanet abaixo imprime o planeta Marte de forma síncrona. Modifique getPlanet, de forma que Marte seja impresso assincronamente, depois de 4 segundos.
+
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  setTimeout(() => console.log("Returned planet: ", mars), 4000);
+};
+
+getPlanet(); // imprime Marte depois de 4 segundos
+
