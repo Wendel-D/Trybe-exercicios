0718b5894e16c22e12c391690b8d1eb7e55ea444
Wendel <wendeldaniel263@gmail.com>
2023-03-20 13:19:58 -0300

    feat: exercicio transpilado

---
+++ dist/Estudantes.js
@@ -0,0 +1,83 @@
+"use strict";
+class Estudantes {
+    constructor(matricula, nome, notaMatematica, notaPortugues, notaGeografia, notaIngles, trabMatematica, trabPortugues) {
+        this._matrícula = matricula;
+        this._nome = nome;
+        this._notaMatematica = notaMatematica;
+        this._notaPortugues = notaPortugues;
+        this._notaGeografia = notaGeografia;
+        this._notaIngles = notaIngles;
+        this._trabMatematica = trabMatematica;
+        this._trabPortugues = trabPortugues;
+    }
+    get matricula() {
+        console.log(this._matrícula);
+        return this._matrícula;
+    }
+    get nome() {
+        console.log(this._nome);
+        return this._nome;
+    }
+    get notaMatematica() {
+        console.log(this._notaMatematica);
+        return this._notaMatematica;
+    }
+    get notaPortugues() {
+        console.log(this._notaPortugues);
+        return this._notaPortugues;
+    }
+    get notaGeografia() {
+        console.log(this._notaGeografia);
+        return this._notaGeografia;
+    }
+    get notaIngles() {
+        console.log(this._notaIngles);
+        return this._notaIngles;
+    }
+    get trabMatematica() {
+        console.log(this._trabMatematica);
+        return this._trabMatematica;
+    }
+    get trabPortugues() {
+        console.log(this._trabPortugues);
+        return this._trabPortugues;
+    }
+    set matricula(matricula) {
+        this._matrícula = matricula;
+    }
+    set nome(nome) {
+        this._nome = nome;
+    }
+    set notaMatematica(notaMatematica) {
+        this._notaMatematica = notaMatematica;
+    }
+    set notaPortugues(notaPortugues) {
+        this._notaPortugues = notaPortugues;
+    }
+    set notaGeografia(notaGeografia) {
+        this._notaGeografia = notaGeografia;
+    }
+    set notaIngles(notaIngles) {
+        this._notaIngles = notaIngles;
+    }
+    set trabMatematica(trabMatematica) {
+        this._trabMatematica = trabMatematica;
+    }
+    set trabPortugues(trabPortugues) {
+        this._trabPortugues = trabPortugues;
+    }
+    sum() {
+        const soma = this._notaGeografia + this._notaIngles + this._notaMatematica + this._notaPortugues + this._trabMatematica + this._trabPortugues;
+        console.log(soma);
+    }
+    avg() {
+        const soma = this._notaGeografia + this._notaIngles + this._notaMatematica + this._notaPortugues + this._trabMatematica + this._trabPortugues;
+        console.log(soma / 6);
+    }
+}
+const wendel = new Estudantes(1, 'Wendel', 10, 9, 3, 4, 5, 10);
+wendel.avg();
+wendel.sum();
+wendel.matricula;
+wendel.nome;
+wendel.notaMatematica;
---
+++ dist/lanchonete.js
@@ -0,0 +1,69 @@
+"use strict";
+class Cliente {
+    constructor(nome) {
+        this._nome = nome;
+    }
+    get nome() {
+        console.log(this._nome);
+        return this._nome;
+    }
+    set nome(nome) {
+        this._nome = nome;
+    }
+}
+class Pedido {
+    constructor(cliente, consumidos, pagamento) {
+        this._desconto = 0;
+        this._cliente = cliente;
+        this._consumidos = consumidos;
+        this._pagamento = pagamento;
+    }
+    get cliente() {
+        console.log(this._cliente);
+        return this._cliente;
+    }
+    get consumidos() {
+        console.log(this._consumidos);
+        return this._consumidos;
+    }
+    get pagamento() {
+        console.log(this._pagamento);
+        return this._pagamento;
+    }
+    get desconto() {
+        console.log(this._desconto);
+        return this._desconto;
+    }
+    set cliente(cliente) {
+        this._cliente = cliente;
+    }
+    set consumidos(consumidos) {
+        this._consumidos = consumidos;
+    }
+    set pagamento(pagamento) {
+        this._pagamento = pagamento;
+    }
+    set desconto(desconto) {
+        this._desconto = desconto;
+    }
+}
+class Item {
+    constructor(nome, preco) {
+        this._nome = nome;
+        this._preco = preco;
+    }
+    get nome() {
+        console.log(this._nome);
+        return this._nome;
+    }
+    get preco() {
+        console.log(this._preco);
+        return this._preco;
+    }
+    set nome(nome) {
+        this._nome = nome;
+    }
+    set preco(preco) {
+        this._preco = preco;
+    }
+}
